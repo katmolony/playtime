bc3c18fc865430d40caec6622df65aa20be572e7
Kate Molony <katemolony96@gmail.com>
2024-02-10 14:54:38 +0000

    track test

--- src/models/mongo/track-mongo-store.js
+++ src/models/mongo/track-mongo-store.js
@@ -1,8 +1,56 @@
 import { Track } from "./track.js";
 
 export const trackMongoStore = {
-  async getTracksByPlaylistId(id) {
+  async getAllTracks() {
+    const tracks = await Track.find().lean();
+    return tracks;
+  },
+
+  async getTrackById(id) {
+    if (id) {
+      const track = await Track.findOne({ _id: id }).lean();
+      return track;
+      }
+      return null;
+    },
+
+  async addTrack(track) {
+    const newTrack = new Track(track);
+    const trackObj = await newTrack.save();
+    return this.getTracksByPlaylistId(trackObj._id);
+  },
+
+    async getTracksByPlaylistId(id) {
     const tracks = await Track.find({ playlistid: id }).lean();
     return tracks;
   },
+
+
+  // async getPlaylistTracks(playlistId) {
+  //   await db.read();
+  //   let foundTracks = tracks.filter((track) => track.playlistid === playlistId);
+  //   if (!foundTracks) {
+  //     foundTracks = null;
+  //   }
+  //   return foundTracks;
+  // },
+
+  // async deleteTrack(id) {
+  //   await db.read();
+  //   const index = db.data.tracks.findIndex((track) => track._id === id);
+  //   if (index !== -1) db.data.tracks.splice(index, 1);
+  //   await db.write();
+  // },
+
+  // async deleteAllTracks() {
+  //   db.data.tracks = [];
+  //   await db.write();
+  // },
+
+  // async updateTrack(track, updatedTrack) {
+  //   track.title = updatedTrack.title;
+  //   track.artist = updatedTrack.artist;
+  //   track.duration = updatedTrack.duration;
+  //   await db.write();
+  // },
 };
