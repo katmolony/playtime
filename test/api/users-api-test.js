0c0ff0932a348222bed67ee5295e82902ad18114
Kate Molony <katemolony96@gmail.com>
2024-02-12 16:21:23 +0000

    adding api tests

--- test/api/users-api-test.js
+++ test/api/users-api-test.js
@@ -1,11 +1,35 @@
+import { assert } from "chai";
 import { playtimeService } from "./playtime-service.js";
+import { assertSubset } from "../test-utils.js";
+import { maggie, testUsers } from "../fixtures.js";
 
 suite("User API tests", () => {
   setup(async () => {
+    await playtimeService.deleteAllUsers();
+    for (let i = 0; i < testUsers.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      testUsers[i] = await playtimeService.createUser(testUsers[i]);
+    }
   });
   teardown(async () => {
   });
 
   test("create a user", async () => {
+    const newUser = await playtimeService.createUser(maggie);
+    assertSubset(maggie, newUser);
+    assert.isDefined(newUser._id);
+  });
+
+  test("delete all users", async () => {
+    let returnedUsers = await playtimeService.getAllUsers();
+    assert.equal(returnedUsers.length, 3);
+    await playtimeService.deleteAllUsers();
+    returnedUsers = await playtimeService.getAllUsers();
+    assert.equal(returnedUsers.length, 0);
+  });
+
+  test("get a user - success", async () => {
+    const returnedUser = await playtimeService.getUser(testUsers[0]._id);
+    assert.deepEqual(testUsers[0], returnedUser);
   });
 });
